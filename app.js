812037f464b0fe2f2b97509f7b24fde0a872e6f1
yenmaylee <yen.lee@bnm.gov.my>
2025-07-21 00:48:25 +0800

    Set up EJS templating and static file serving

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
+
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+app.use(express.static('public'));
+
+// Templating engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');

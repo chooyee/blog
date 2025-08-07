49ddd36e53d29c2d11592dfee7bd4745ba5b5bdb
yenmaylee <yen.lee@bnm.gov.my>
2025-08-07 23:39:10 +0800

    Pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,17 +4,39 @@ const Post = require('../models/Post');
 
 //Routes
 router.get('/', async (req, res) => {
-    const locals = {
-        title: 'Home',
-        description: 'Welcome to the blog!'
-    }
 
     // Fetch posts from the database
     // and render the index view with the posts data
     try {
-      const data = await Post.find();
-      res.render('index', { locals, data });
+      const locals = {
+          title: 'Home',
+          description: 'Welcome to the blog!'
+      }
 
+      let perPage = 10;
+      let page = req.query.page || 1; // get the page or default to 1
+
+      // Use aggregate to sort, skip, and limit the posts
+      const data = await Post.aggregate([
+        { $sort: { createdAt: -1 } }, // sort by createdAt in descending order
+        { $skip: (perPage * page) - perPage }, // skip the first N documents
+        { $limit: perPage } // return the next 10 documents
+      ]);
+      
+      const count = await Post.countDocuments(); // Count total posts for pagination
+      const nextPage = parseInt(page) + 1; // calculate the next page number
+      const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+      res.render('index', {
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null,
+      });
+
+      // const data = await Post.find();
+      // res.render('index', { locals, data });
+      
     } catch (error) {
       console.log(error)
     }
@@ -67,7 +89,7 @@ function insertPostData () {
   ])
 }
 
-// insertPostData();
+insertPostData();
 
 router.get('/about', (req, res) => {
     res.render('about');
